cb90cff0f0f2bd1d6a0e03cfa968734aa38f42d5
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2017-06-29 23:55:25 +0300

    Add HomeController with rest

--- das-boot/src/main/java/com/boot/controller/HomeController.java
+++ das-boot/src/main/java/com/boot/controller/HomeController.java
@@ -1,13 +1,41 @@
 package com.boot.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boot.model.Shipwreck;
+
 @RestController
+@RequestMapping("api/v1/")
 public class HomeController {
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+    public Shipwreck get(@PathVariable long id) {
+        return ShipwreckStub.get(id);
+    }
     
-    @RequestMapping("/")
-    public String home(){
-        return "DAs Boot, reporting for duty!";
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+    public List<Shipwreck> get() {
+        return ShipwreckStub.list();
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
+    public Shipwreck delete(@PathVariable long id) {
+        return ShipwreckStub.delete(id);
+    }
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
+    public Shipwreck post(@RequestBody Shipwreck shipwreck) {
+        return ShipwreckStub.create(shipwreck);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
+    public Shipwreck update(@RequestBody Shipwreck shipwreck, @PathVariable long id) {
+        return ShipwreckStub.update(id, shipwreck);
     }
 }
